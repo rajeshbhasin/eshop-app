46e532ab871b6865233fbbcca4c57ad9bf8ce74e
Rajesh Bhasin <rajesh045@gmail.com>
2023-08-18 21:30:45 +0530

    Update the homepage image

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,11 +2,12 @@ import React from 'react';
 import Product from './Product';
 import  '../styles/Home.css';
 
+
 function Home() {
   return (
       <div className="home">
         <div className="home__container">
-        <img src="https://www.x-cart.com/wp-content/uploads/2019/01/ecommerce-768x278.jpg" alt="home__image" className="home__image"/>
+        <img src={require('./Ecomm2.png')}  alt="home__image" className="home__image"/>
         <div className="home__row">
             <Product 
             id="1"
